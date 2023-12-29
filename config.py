b4ea25d1d9e792b16e120cc81afd57005c7f6d9b
Le Duc Minh <minhmeo7a1@gmail.com>
2023-12-30 01:13:07 +0700

    Add kaggle

---
+++ config.py
@@ -0,0 +1,9 @@
+# TRAIN_DIRS = ['data/train_1', 'data/train_2', 'data/train_3']
+# TEST_DIRS = ['data/test']
+# VAL_DIRS = ['data/valid']
+
+
+
+TRAIN_DIRS = ['/kaggle/input/deep-learning-hust/train_1', '/kaggle/input/deep-learning-hust/train_2', '/kaggle/input/deep-learning-hust/train_3']
+TEST_DIRS = ['/kaggle/input/deep-learning-hust/test']
+VAL_DIRS = ['/kaggle/input/deep-learning-hust/valid']
